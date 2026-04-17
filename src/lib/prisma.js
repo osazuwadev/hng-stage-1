43a880f90027a40188606cd647bcdf0136a7ef9d
osazuwadev <osazuwa.dev@gmail.com>
2026-04-18 00:31:59 +0100

    fix: clean up prisma.js duplicate code

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,11 +1,6 @@
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient();
-
-export default prisma;import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-
 const prisma = new PrismaClient({
     datasources: {
         db: {
