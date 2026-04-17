7d9d9912dacfe7aff58e848b51b761917ed136f3
osazuwadev <osazuwa.dev@gmail.com>
2026-04-17 23:26:29 +0100

    fix: move dotenv config to top

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors"
-import dotenv from "dotenv";
 import profilesRoutes from "./routes/profiles.js";
 
-dotenv.config()
 const app = express();
 
 app.use(cors({ origin: "*"}));
