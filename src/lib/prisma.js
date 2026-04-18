fe2406e05cb06ed55d97d260075dc9036a8b3336
osazuwadev <osazuwa.dev@gmail.com>
2026-04-18 01:01:56 +0100

    fix: hardcode database URL temporarily

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,12 @@
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: "postgresql://neondb_owner:npg_z2ISoejVubq1@ep-proud-lab-amrlzgjv-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+        }
+    }
+});
 
 export default prisma;
